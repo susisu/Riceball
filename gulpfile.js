36a0a2c0c67f2193f27b23fe06ad2abeed6689f3
Susisu <susisu2413@yahoo.co.jp>
2015-08-04 13:38:04 +0900

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
-var gulp    = require("gulp"),
-    webpack = require("gulp-webpack");
+var gulp       = require("gulp"),
+    gulpif     = require("gulp-if"),
+    webpack    = require("gulp-webpack"),
+    preprocess = require("gulp-preprocess");
 
 var packageInfo = require("./package.json");
 var banner      = "Riceball " + packageInfo.version + "\n\
 copyright (c) 2015 Susisu | MIT License\n\
 https://github.com/susisu/Riceball";
 
-gulp.task("webpack", function () {
-    return gulp.src("./src/riceball.js")
+gulp.task("debug-build-js", function () {
+    return gulp.src("./src/js/riceball.js")
         .pipe(webpack({
             "output": {
                 "libraryTarget": "var",
@@ -16,41 +18,42 @@ gulp.task("webpack", function () {
                 "filename"     : "riceball.js"
             },
             "externals": {
-                "loquat": true
+                "window" : true,
+                "iceball": true,
+                "pixi"   : "PIXI"
             },
             "plugins": [
                 new webpack.webpack.BannerPlugin(
                     banner,
                     { "entryOnly": true }
-                )
+                ),
+                new webpack.webpack.DefinePlugin({
+                    "DEBUG": true
+                })
             ]
         }))
-        .pipe(gulp.dest("./dist/js"));
+        .pipe(gulp.dest("./debug/js"));
 });
 
-gulp.task("webpack-min", function () {
-    return gulp.src("./src/riceball.js")
-        .pipe(webpack({
-            "output": {
-                "libraryTarget": "var",
-                "library"      : "riceball",
-                "sourcePrefix" : "    ",
-                "filename"     : "riceball.min.js",
-            },
-            "externals": {
-                "loquat": true
-            },
-            "plugins": [
-                new webpack.webpack.optimize.UglifyJsPlugin(),
-                new webpack.webpack.BannerPlugin(
-                    banner,
-                    { "entryOnly": true }
-                )
-            ]
-        }))
-        .pipe(gulp.dest("./dist/js"));
+gulp.task("debug-build-css", function () {
+    return gulp.src("./src/css/*.css")
+        .pipe(gulp.dest("./debug/css"));
 });
 
-gulp.task("build", ["webpack", "webpack-min"]);
+gulp.task("debug-build-pages", function () {
+    return gulp.src("./src/pages/**/*")
+        .pipe(gulpif("*.html", preprocess({ "context": { "DEBUG": true } })))
+        .pipe(gulp.dest("./debug"));
+});
 
-gulp.task("default", ["build"]);
+gulp.task("debug-build", ["debug-build-js", "debug-build-css", "debug-build-pages"]);
+
+gulp.task("watch", function () {
+    gulp.watch("./src/js/**/*", ["debug-build-js"]);
+    gulp.watch("./src/css/**/*", ["debug-build-css"]);
+    gulp.watch("./src/pages/**/*", ["debug-build-pages"]);
+});
+
+gulp.task("debug", ["debug-build", "watch"]);
+
+gulp.task("default", ["debug-build"]);
